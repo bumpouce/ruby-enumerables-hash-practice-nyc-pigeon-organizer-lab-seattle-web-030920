3cd77b1390665518e0c336581b1fa795249df1af
Christine Bumpous <bumpouce@gmail.com>
2020-01-30 00:21:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -24,6 +24,6 @@ def nyc_pigeon_organizer(data)
     end
   end
 
-  reorganized_pigeon_data.collect {|individual| pp "#{individual}"
+#  reorganized_pigeon_data.collect {|individual| pp "#{individual}"
   reorganized_pigeon_data
 end
